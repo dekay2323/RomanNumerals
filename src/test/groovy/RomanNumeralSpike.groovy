1dcfc2e45e0e64f3530afc5c37130fca95bfde8b
DemianK <demiankrige@gmail.com>
2016-09-21 20:40:35 -0600

    Spock framework quick start https://github.com/spockframework/spock-example

---
+++ src/test/groovy/RomanNumeralSpike.groovy
@@ -0,0 +1,13 @@
+import spock.lang.Specification
+
+/**
+ * Created by demian on 2016-09-21.
+ */
+class RomanNumeralSpike extends Specification {
+
+    def "Roman Numerals solver"() {
+        when:
+        then:
+        false
+    }
+}
