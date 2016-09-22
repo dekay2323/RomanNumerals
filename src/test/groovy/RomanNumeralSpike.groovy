98e679d0f38e5c857ae1676f329f2bd00990fbf5
DemianK <demiankrige@gmail.com>
2016-09-21 20:47:21 -0600

    Simple roman numeral test failing in spike

--- src/test/groovy/RomanNumeralSpike.groovy
+++ src/test/groovy/RomanNumeralSpike.groovy
@@ -1,13 +1,26 @@
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by demian on 2016-09-21.
  */
 class RomanNumeralSpike extends Specification {
 
-    def "Roman Numerals solver"() {
-        when:
-        then:
-        false
+    def romanSolver = {value ->
+        'WRONG'
+    }
+
+    @Unroll
+    def "Roman Numerals solver (non subtractive) #decimal = #romanNumeral"() {
+        expect:
+        romanSolver(decimal) == romanNumeral
+
+        where:
+        decimal     || romanNumeral
+        1           || 'I'
+        2           || 'II'
+        3           || 'III'
+        4           || 'IIII'
+        5           || 'V'
     }
 }
