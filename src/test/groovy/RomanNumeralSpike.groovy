608bfab1aecd9c9980533843793f1369bb31c219
DemianK <demiankrige@gmail.com>
2016-09-21 22:26:15 -0600

    Many more tests, subtractive roman numerals and massive simpliciation

--- src/test/groovy/RomanNumeralSpike.groovy
+++ src/test/groovy/RomanNumeralSpike.groovy
@@ -3,42 +3,26 @@ import spock.lang.Unroll
 
 /**
  * Quick Spike at a Roman Numerals Solver.
- * Non Subtractive first then subtractive
+ * Starting to use subtractive
  *
  * https://en.wikipedia.org/wiki/Roman_numerals
  *
  * Created by demian on 2016-09-21.
  */
 class RomanNumeralSpike extends Specification {
-    def romanNumeralValues = [M:1000, D:500, C:100, L:50, X:10, V:5, I:1]
+    def romanNumeralValues = [M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1]
 
     def toRomanNumeral = {anInteger ->
-        def buckets = [:]
-        buckets = romanNumeralValues.collectEntries  { pair->
-            [(pair.key):0]
-        }
-        println "inital buckets = ${buckets}"
-        println "decimal = ${anInteger}"
+        def ans = ""
         romanNumeralValues.each { pair ->
-            println "charVal.value = ${pair.value}"
-            println "anInteger/charVal.value = ${anInteger/pair.value}"
-            println "anInteger%charVal.value = ${anInteger%pair.value}"
             if (anInteger/pair.value >= 1) {
-                buckets.(pair.key) += anInteger.intdiv(pair.value)
+                ans += "${pair.key}" * anInteger.intdiv(pair.value)
                 anInteger = anInteger - (anInteger.intdiv(pair.value)*pair.value)
-                println "new anInteger = ${anInteger}"
-            }
-            println "buckets = ${buckets}"
-        }
-        def answer = buckets.inject("") { result, pair ->
-            if (pair.value > 0) {
-                result += "${pair.key}" * pair.value
-            } else {
-                result
             }
         }
-        answer
+        ans
     }
+
 
     @Unroll
     def "Roman Numerals solver (non subtractive) #anInteger = #romanNumeral"() {
@@ -46,21 +30,30 @@ class RomanNumeralSpike extends Specification {
         toRomanNumeral(anInteger) == romanNumeral
 
         where:
-        anInteger     || romanNumeral
+        anInteger   || romanNumeral
         1           || 'I'
         2           || 'II'
         3           || 'III'
-        4           || 'IIII'
+        4           || 'IV'
         5           || 'V'
         6           || 'VI'
         7           || 'VII'
         8           || 'VIII'
-        9           || 'VIIII'
+        9           || 'IX'
         10          || 'X'
         11          || 'XI'
         12          || 'XII'
         13          || 'XIII'
-        14          || 'XIIII'
+        14          || 'XIV'
         15          || 'XV'
+        49          || 'XLIX'
+        50          || 'L'
+        51          || 'LI'
+        99          || 'XCIX'
+        100         || 'C'
+        101         || 'CI'
+        500         || 'D'
+        1000        || 'M'
+        2016        || 'MMXVI'
     }
 }
